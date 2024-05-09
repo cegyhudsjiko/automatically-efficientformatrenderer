394d5daffca320d8e6d944a50de927876363cead
cegyhudsjiko <bsjclablabla727@hotmail.com>
2024-05-09 17:04:25 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const aliceBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: aliceAddress,
-  minimumLedgerVersion: BigInt(response.version),
-});
-const bobBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: bobAddress,
-});
-const sponsorBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: sponsorAddress,
-});
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+};
