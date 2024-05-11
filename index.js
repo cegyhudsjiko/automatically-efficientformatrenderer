c3dfcd781390f28fe77dfd6ef83af855f7260c25
cegyhudsjiko <bsjclablabla727@hotmail.com>
2024-05-11 18:32:25 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const bubbleSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
+function multiply(num1, num2) {
+  const m = num1.length;
+  const n = num2.length;
+  const pos = new Array(m + n).fill(0);
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      const mul = (num1[i] - "0") * (num2[j] - "0");
+      const p1 = i + j;
+      const p2 = i + j + 1;
+      const sum = mul + pos[p2];
+      pos[p1] += Math.floor(sum / 10);
+      pos[p2] = sum % 10;
     }
   }
-  return arr;
-};
+  let result = "";
+  for (const p of pos) {
+    if (!(result.length === 0 && p === 0)) result += p;
+  }
+  return result.length === 0 ? "0" : result;
+}
