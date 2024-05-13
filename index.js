26b2918943e1db9eee71bd747b50bdb933a0a423
cegyhudsjiko <bsjclablabla727@hotmail.com>
2024-05-13 14:57:31 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-function numIslands(grid) {
-  if (!grid || grid.length === 0) return 0;
-  const m = grid.length;
-  const n = grid[0].length;
-  let islands = 0;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (grid[i][j] === "1") {
-        islands++;
-        dfs(grid, i, j);
-      }
-    }
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
   }
-  return islands;
-}
-function dfs(grid, i, j) {
-  const m = grid.length;
-  const n = grid[0].length;
-  if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === "0") return;
-  grid[i][j] = "0";
-  dfs(grid, i + 1, j);
-  dfs(grid, i - 1, j);
-  dfs(grid, i, j + 1);
-  dfs(grid, i, j - 1);
+  return maxProfit;
 }
